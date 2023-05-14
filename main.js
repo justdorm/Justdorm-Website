e1cb0a66151bb096a8101ff595cfe5f84df70d97
Justin Dormitzer <justin.dormitzer@gmail.com>
2023-05-13 20:56:28 -0400

    fixed button hover

--- main.js
+++ main.js
@@ -36,11 +36,16 @@ var DesignGrid = document.getElementById("design-grid");
 var AnimBtn = document.getElementById("anim-btn");
 var DesignBtn = document.getElementById("design-btn");
 
+var animogcolor;
+var dsgogcolor;
+
 AnimBtn.onclick = function(event){
     AnimBtn.style.color = "#e74d83";
     DesignBtn.style.color ="#fff";
     DesignGrid.style.display="none";
     AnimGrid.style.display="flex";
+    animogcolor="#e74d83";
+    dsgogcolor="white";
     masonry.layout();
 
 
@@ -50,7 +55,36 @@ DesignBtn.onclick = function(event){
     AnimBtn.style.color="#fff";
     AnimGrid.style.display="none";
     DesignGrid.style.display="flex";
+    dsgogcolor="#e74d83";
+    animgogcolor="white";
     masonry2.layout();
 
 
 };
+
+
+AnimBtn.onmouseenter = function(event){
+    console.log(animogcolor);
+    animogcolor=AnimBtn.style.color;
+    AnimBtn.style.color="white";
+}
+
+AnimBtn.onmouseleave = function(event){
+    console.log(animogcolor);
+    if (animogcolor){
+        AnimBtn.style.color=animogcolor;
+    }
+}
+
+
+DesignBtn.onmouseenter = function(event){
+    dsgogcolor=DesignBtn.style.color;
+    DesignBtn.style.color="white";
+}
+
+DesignBtn.onmouseleave = function(event){
+    if (dsgogcolor){
+        DesignBtn.style.color=dsgogcolor;
+    }
+}
+
